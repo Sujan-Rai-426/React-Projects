e9f03d17f3129a7787aa1d59e63eee36eafb50ba
Sujan <rsujan140.in@gmail.com>
2024-12-10 23:12:09 +0545

    Button to increase or decrease counter ,  02_Counter_Button/

---
+++ 02_Counter_Button/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  //
+
+  return (
+    <>
+      
+      <h1> Simple Counter button  </h1>
+      <h2>For React learning Purpose </h2>
+      <br />
+      <h1>Count : {count}</h1>
+      <br />
+
+      <button onClick={()=> setCount(count+1) }  style={{background: 'gray', color: 'white'}} > Counter Up </button> {"   "}
+      <button onClick={()=> setCount(count-1) } style={{background: 'gray', color: 'white'}} > Counter Down </button>
+    </>
+  )
+}
+
+export default App
