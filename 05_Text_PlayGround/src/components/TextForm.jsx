912a9ea946fa69326a1261be342d6a22db8054d3
Sujan <rsujan140.in@gmail.com>
2024-12-18 15:16:11 +0545

    TextPayground minor components edit

--- 05_Text_PlayGround/src/components/TextForm.jsx
+++ 05_Text_PlayGround/src/components/TextForm.jsx
@@ -8,7 +8,7 @@ function TextForm(props) {
 
 
     // Initialize the text and setText
-    const [text, setText] = useState ("Enter your text here to customize...");
+    const [text, setText] = useState ("");
 
     // function to handel the text change event
     const handle_change = (event) =>{
@@ -17,11 +17,13 @@ function TextForm(props) {
 
     // function to handle the button click events
     const handle_Upper_click = () => {
-        setText(text.toUpperCase())
+        let upr_txt = text.toUpperCase()
+        setText(upr_txt)
     }
     
     const handle_Lower_click = () => {
-        setText(text.toLowerCase())
+        let lwr_txt = text.toLowerCase()
+        setText(lwr_txt)
     }
 
     const textRef = useRef(null)
@@ -38,10 +40,12 @@ function TextForm(props) {
 
 
     return (
-        
+    
+    <>
+    {/* start div container for text operation */}
         <div className = 'my-5'>
             <div style={{display: 'flex', justifyContent:'space-between'}}>
-                <h2> {props.heading} </h2>
+                <h3> {props.heading} </h3>
                 {/* <div>
                     <label htmlFor="exampleColorInput" className="form-label"> <h5> Color picker </h5></label>
                     <input type="color" className="form-control form-control-color" id="exampleColorInput" value="#563d7c" title="Choose your color"></input>
@@ -53,14 +57,25 @@ function TextForm(props) {
             </div>
 
             {/* button div */}
-            <div style={{ display: 'flex', gap :'3vw' }}>
-                <button onClick={handle_Upper_click} className="btn btn-success"> convert to Uppercase </button>
-                <button onClick={handle_Lower_click} className="btn btn-success"> convert to Lowercase </button>
-                <button onClick={handle_copy} className="btn btn-success"> copy text </button>
-                <button onClick={handle_clear} className="btn btn-success"> clear text </button>
+            <div style={{ display: 'flex', gap :'2vw' }}>
+                <button onClick={handle_Upper_click} className="btn btn-danger"> Uppercase </button>
+                <button onClick={handle_Lower_click} className="btn btn-danger"> Lowercase </button>
+                <button onClick={handle_copy} className="btn btn-danger"> Copy Text </button>
+                <button onClick={handle_clear} className="btn btn-danger"> Clear Text </button>
             </div>
         </div>
-
+    {/* end div container for text operation */}
+    
+    
+    {/* start div container for text information */}
+        <div className="container">
+            <h3> Text Summary </h3>
+            <p> Total word = <b> {text.split(" ").length} </b>, Total characters in text = <b> {text.length} </b></p>
+            <h3> Preview </h3>
+            <p> <i> {text}</i> </p>
+        </div>
+    {/* end div container for text information */}
+    </>
     )
 }
 
